0f051526b291ff9f17f4321dd211136b4de07e31
ale.pissotti@gmail.com <ale.pissotti@gmail.com>
2020-10-12 17:21:08 -0300

    Agregando la ruta de los productos y creando platillas

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,17 @@
+@extends('header.header')
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Listado de productos
+                        <a href="" class="btn btn-sm btn-success float-right">Agregar producto</a>
+                    </div>
+                    <div class="card-body">
+                        ...
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@extends('footer.footer')
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('products', function () {
+    return view('products.index');
 });
